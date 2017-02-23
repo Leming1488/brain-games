b79012959f7c8e8168b93be6467a28cfefadcc9e
Pavel <borovweb@gmail.com>
2017-02-23 21:03:37 +0300

    Update balance.js

--- src/games/balance.js
+++ src/games/balance.js
@@ -9,20 +9,15 @@ const expression = () => {
 
 const balance = (number) => {
   const numbers = `${number}`.split('').map(el => parseInt(el, 10));
-  
   const maxFromArr = arr => Math.max(...arr);
   const minFromArr = arr => Math.min(...arr);
-  
   const dicrease = (arr) => {
     const newArr = arr.slice();
-    
     const max = maxFromArr(newArr);
     const min = minFromArr(newArr);
-    
     if (max - min > 1) {
       newArr.splice(newArr.indexOf(max), 1, max - 1);
       newArr.splice(newArr.indexOf(min), 1, min + 1);
-      
       return dicrease(newArr);
     }
     return newArr.sort((a, b) => a - b).join('');
